bfdea08939be2a49e97df9135e75c5c033c131b6
sschonss <schonsluuiz@gmail.com>
2023-05-30 00:46:51 -0300

    Update public function authorize() to 'false'

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreClientRequest extends FormRequest
@@ -11,18 +12,20 @@ class StoreClientRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|unique:users|email|max:255',
+            'address_id' => 'required'
         ];
     }
 }
